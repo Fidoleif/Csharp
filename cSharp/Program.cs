cb85503ae9cf9dc7a49e7f5b72e93dbd8194d809
Michael Nilsson <michael.nilsson@wippies.se>
2013-09-16 14:50:12 +0200

    I am your new master!

--- cSharp/Program.cs
+++ cSharp/Program.cs
@@ -10,6 +10,8 @@ namespace cSharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello git");
+            Console.WriteLine("Hello Master");
         }
     }
 }
